5407356c2dd76ecb2be7d11ae4bea552f4b22e1e
Konno Daichi <m5291041@u-aizu.ac.jp>
2025-06-23 14:02:49 +0900

    Add files and folders

---
+++ build/test_piece/CMakeFiles/test_piece.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_piece.dir/src/crack_detection.cpp.o"
+  "CMakeFiles/test_piece.dir/src/nn/autobackend.cpp.o"
+  "CMakeFiles/test_piece.dir/src/nn/onnx_model_base.cpp.o"
+  "CMakeFiles/test_piece.dir/src/utils/augment.cpp.o"
+  "CMakeFiles/test_piece.dir/src/utils/common.cpp.o"
+  "CMakeFiles/test_piece.dir/src/utils/ops.cpp.o"
+  "test_piece"
+  "test_piece.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_piece.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
